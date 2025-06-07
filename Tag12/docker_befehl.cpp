44617c208f3cebc1af7f40e1f52af3b2835f93d9
Aboudou Koffitse <62890086+aboudou123@users.noreply.github.com>
2025-06-07 16:19:22 +0200

    Add files via upload

---
+++ Tag12/docker_befehl.cpp
@@ -0,0 +1,43 @@
+koffi@DePatrick:~$ history
+    1  cd Download
+    2  cd Downloads
+    3  chmod ssh -i "docker-project-key.pem" ubuntu@ec2-54-160-187-164.compute-1.amazonaws.com
+    4  chmod 400 docker-project-key.pem
+    5  pwd
+    6  ls -l
+    7  find ~ -name "docker-project-key.pem"
+    8  chmod 400 docker-project-key.pem
+    9  ls
+   10  cd Desktop
+   11  cd downloads
+   12  cd
+   13  cd Desktop
+   14  pwd
+   15  cd home
+   16  cd /koffi
+   17  clear
+   18  cd ~/Desktop
+   19  ls
+   20  ls -l ~
+   21  mkdir ~/Desktop
+   22  ls -l
+   23  cd ~/Desktop
+   24  mv /path/to/docker-project-key.pem ~/Desktop/
+   25  mv /C:\Users\patri\Downloads\docker-project-key.pem ~/Desktop/
+   26  /mnt/c/Users/patri/Downloads/docker-project-key.pem
+   27  mv /mnt/c/Users/patri/Downloads/docker-project-key.pem ~/Desktop/
+   28  ls /mnt/c/Users/patri/Downloads/
+   29  ls
+   30  chmod 400 ssh -i "docker-project-key.pem" ubuntu@ec2-54-160-187-164.compute-1.amazonaws.com
+   31  chmod 400 ssh -i docker-project-key.pem
+   32  chmod 400 docker-project-key.pem
+   33  ssh -i "docker-project-key.pem" ubuntu@ec2-54-160-187-164.compute-1.amazonaws.com
+   34  history
+   35  cd --
+   36  history
+   
+   
+git init
+
+git add main.py
+git commit -m "Initiale Version von main.py"
